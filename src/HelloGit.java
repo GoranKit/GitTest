74ce4496a7069f63c86249ceda00bc52aa253f61
Goran Chow <goran.zhou@astasys.com>
2017-08-30 11:28:36 +0800

    master2.0

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,16 +1,7 @@
-/**
- * Created by user on 8/16/2017.
- */
 public class HelloGit {
 
     public static void main(String[] args) {
 
         System.out.println("Hello Git!!!");
-        System.out.println("B teste");
-        System.out.println("A one");
-        System.out.println("B two");
-        System.out.println("A bug fix");
-        System.out.println("edit branch of  bug fix");
-        System.out.println("edit branch of  bug fix");
     }
 }
