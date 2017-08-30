eaf9f75245795e02610c203e6b163bc3f2a96f04
Goran Chow <goran.zhou@astasys.com>
2017-08-30 11:57:01 +0800

    master4.0

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -2,6 +2,7 @@ public class HelloGit {
 
     public static void main(String[] args) {
 
-        System.out.println("????");
+        System.out.println("22");
+        System.out.println("??344??");
     }
 }
